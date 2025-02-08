a3a4fe8f8f2c016f3ec4e46debdae5d1d3f08cbf
HYK <junhyeok001010@gmail.com>
2025-02-08 19:44:39 +0900

    implemented api-key authentication

--- src/main/java/com/hyk/hykportfolioback/config/SecurityConfig.java
+++ src/main/java/com/hyk/hykportfolioback/config/SecurityConfig.java
@@ -1,18 +1,32 @@
 package com.hyk.hykportfolioback.config;
 
+import com.hyk.hykportfolioback.filter.APIKeyAuthenticationFilter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.io.IOException;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+  @Value("${auth.api-key}")
+  private String apiKey;
 
   @Bean
   protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -21,8 +35,13 @@ public class SecurityConfig {
         .cors(cors -> cors
             .configurationSource(corsConfigurationSource()))
         .authorizeHttpRequests(authorize -> authorize
-            .anyRequest().permitAll()
-        );
+            .requestMatchers(HttpMethod.GET, "/api/v1/project/**").permitAll()
+            .anyRequest().authenticated()
+        )
+        .exceptionHandling((exceptionHandling) -> exceptionHandling
+            .authenticationEntryPoint(new FailedAuthenticationEntryPoint())
+        )
+        .addFilterBefore(new APIKeyAuthenticationFilter(apiKey), UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
   }
@@ -38,6 +57,17 @@ public class SecurityConfig {
     source.registerCorsConfiguration("/**", configuration);
 
     return source;
+  }
+
+  private static class FailedAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+      response.setContentType("application/json");
+      response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+      response.getWriter().write("{\"code\": \"AF\", \"message\": \"Authorization Failed.\"}");
+    }
+
   }
 
 }
---
+++ src/main/java/com/hyk/hykportfolioback/filter/APIKeyAuthenticationFilter.java
@@ -0,0 +1,61 @@
+package com.hyk.hykportfolioback.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class APIKeyAuthenticationFilter extends OncePerRequestFilter {
+
+  private final String expectedAPIKey;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    String receivedAPIKey = request.getHeader("API-KEY");
+    System.out.println("receivedAPIKey: " + receivedAPIKey);
+    System.out.println("expectedAPIKey: " + expectedAPIKey);
+
+    if (expectedAPIKey.equals(receivedAPIKey)) {
+      Authentication authentication = new APIKeyAuthentication(receivedAPIKey);
+      SecurityContextHolder.getContext().setAuthentication(authentication);
+
+      filterChain.doFilter(request, response);
+      return;
+    }
+
+    filterChain.doFilter(request, response);
+  }
+
+  private static class APIKeyAuthentication extends AbstractAuthenticationToken {
+
+    private final String apiKey;
+
+    public APIKeyAuthentication(String apiKey) {
+      super(AuthorityUtils.NO_AUTHORITIES);
+      this.apiKey = apiKey;
+      setAuthenticated(true);
+    }
+
+
+    @Override
+    public Object getCredentials() {
+      return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+      return apiKey;
+    }
+
+  }
+
+}

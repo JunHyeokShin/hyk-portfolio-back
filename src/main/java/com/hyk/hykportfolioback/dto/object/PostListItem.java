1b08f3d5798eccf2c64b900acb63cdbb0d7235ee
HYK <junhyeok001010@gmail.com>
2025-03-24 11:39:57 +0900

    add viewCount, updatedAt fields to PostListItem

--- src/main/java/com/hyk/hykportfolioback/dto/object/PostListItem.java
+++ src/main/java/com/hyk/hykportfolioback/dto/object/PostListItem.java
@@ -17,14 +17,18 @@ public class PostListItem {
   private String thumbnail;
   private String themeColor;
   private List<String> tags;
+  private int viewCount;
   private String createdAt;
+  private String updatedAt;
 
   public PostListItem(PostWithTagsEntity postWithTagsEntity) {
     this.id = postWithTagsEntity.getId();
     this.title = postWithTagsEntity.getTitle();
     this.thumbnail = postWithTagsEntity.getThumbnail();
     this.themeColor = postWithTagsEntity.getThemeColor();
+    this.viewCount = postWithTagsEntity.getViewCount();
     this.createdAt = postWithTagsEntity.getCreatedAt();
+    this.updatedAt = postWithTagsEntity.getUpdatedAt();
     try {
       ObjectMapper objectMapper = new ObjectMapper();
       this.tags = objectMapper.readValue(postWithTagsEntity.getTags(), new TypeReference<List<String>>() {

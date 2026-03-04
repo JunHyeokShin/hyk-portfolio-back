93f8be292df00af89dca98ec644d2e649b9ddfb1
HYK <junhyeok001010@gmail.com>
2026-03-04 18:46:43 +0900

    프로젝트 초기화

---
+++ src/main/java/com/hyk/portfolio/HykPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.hyk.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HykPortfolioApplication {
+
+  static void main(String[] args) {
+    SpringApplication.run(HykPortfolioApplication.class, args);
+  }
+
+}

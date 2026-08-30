16479825dd7eba0caee8020e0ce215e686e1c383
HYK <junhyeok001010@gmail.com>
2026-08-31 00:12:48 +0900

    chore: 프로젝트 초기화

---
+++ src/test/java/com/hyk/portfolio/HykPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyk.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HykPortfolioApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

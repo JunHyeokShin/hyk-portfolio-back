202fcea639a76d2e6158d7c946d512aa6cc8cf48
HYK <junhyeok001010@gmail.com>
2026-09-01 02:00:05 +0900

    docs: SaveProjectPort의 예외 계약 주석 제거

--- src/main/java/com/hyk/portfolio/project/application/port/out/SaveProjectPort.java
+++ src/main/java/com/hyk/portfolio/project/application/port/out/SaveProjectPort.java
@@ -4,9 +4,6 @@ import com.hyk.portfolio.project.domain.model.Project;
 
 public interface SaveProjectPort {
 
-  /**
-   * slug가 이미 사용 중이면 SLUG_DUPLICATED로 실패한다.
-   */
   Project save(Project project);
 
 }
